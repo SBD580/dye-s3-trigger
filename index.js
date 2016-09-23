eac00dd9b262a844c2b3eff14c85614c63da0702
Shay Ben David <Shay Ben David>
2016-09-24 00:42:22 +0300

    no message

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 var aws = require('aws-sdk');
 var vision = require('node-cloud-vision-api');
 
+var FOOD_LABELS = ['fish','milk','bread'];
+var FOOD_LABEL_MIN_SCORE = 0.5;
+var DETECTION_MAX_LABELS = 100;
+
+var DYNAMO_TABLE = 'dynamic-yield-exercise';
+var DYNAMO_PK = 'cat';
+var LAST_FOOD_TIME_ATTR = 'LAST_FOOD_TIME';
+
 var s3 = new aws.S3({apiVersion: '2006-03-01'});
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 vision.init({auth: 'AIzaSyCMHDy3VglnLi72j-MYxFvQB413deiZDZw'});
 
@@ -29,11 +38,49 @@ exports.handler = function(event, context, callback){
         vision.annotate(new vision.Request({
             image: new vision.Image({base64: data.Body.toString('base64')}),
             features: [
-                new vision.Feature('LABEL_DETECTION', 100),
+                new vision.Feature('LABEL_DETECTION', DETECTION_MAX_LABELS)
             ]
         })).then(function(res){
-            console.log(JSON.stringify(res));
-            callback(res);
+            console.log("Image process by Google Vision API successfully");
+
+            if(!res.responses || !res.responses.length || !res.responses[0].labelAnnotations){
+                return callback('Bad response from Google Vision API');
+            }
+
+            var labels = res.responses[0].labelAnnotations;
+            console.log("Detected "+labels.left+" labels");
+            console.log(labels);
+
+            for(var i=0;i<labels.length;i++){
+                var lbl = labels[i];
+                if(lbl.score>=FOOD_LABEL_MIN_SCORE && FOOD_LABELS.indexOf(lbl.description)>=0){
+                    console.log('Found a FOOD label with the required minimal score! ('+lbl.description+')');
+
+                    dynamodb.updateItem({
+                        TableName: DYNAMO_TABLE,
+                        Key: {
+                            key: {S: DYNAMO_PK}
+                        },
+                        UpdateExpression: 'SET '+LAST_FOOD_TIME_ATTR+' = :time',
+                        ExpressionAttributeValues: {
+                            ':time': {N: Date.now()}
+                        }
+                    },function(err,data){
+                        if(err){
+                            console.error('Failed to update last food time');
+                            return callback(err);
+                        }
+                        console.log('Last food time was updated successfully');
+
+                        callback();
+                    });
+                    return;
+                }
+            }
+
+            console.log('No food label found');
+
+            callback();
         },function(err){
             console.error(err);
             callback(err);
